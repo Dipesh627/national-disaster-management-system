83a104207f274bd4d4851819e348ca157ba9c1ed
Dip Esh <dipeshrauted@gmail.com>
2026-09-19 21:33:58 +0545

    Fix static files for production

--- ndms/settings.py
+++ ndms/settings.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -185,6 +186,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (user-uploaded incident photos)
 # https://docs.djangoproject.com/en/6.0/topics/files/
